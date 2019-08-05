062ce9b014b7c9b8bd45e28beeb62be9a8292527
Joshua Peeling <jpeeling13@outlook.com>
2019-08-05 00:07:28 -0400

    fixed logic errors, added params for moving averages

--- multiplestocks_crossover_strategy.py
+++ multiplestocks_crossover_strategy.py
@@ -9,15 +9,17 @@ import backtrader as bt
 # Create our first test strategy!!
 class TestStrategy(bt.Strategy):
 
+    params = (('sma1', 40),('sma2', 100),);
+
     def __init__(self):
-        print("Creating SMA40, SMA100, and CROSS for each Datafeed");
+        print("Creating sma1, sma2, and CROSS for each Datafeed");
         self.myInds = dict();
 
         for datafeed in self.datas:
             self.myInds[datafeed] = dict();
-            self.myInds[datafeed]["sma40"] = bt.indicators.SimpleMovingAverage(datafeed.close, period = 40);
-            self.myInds[datafeed]["sma100"] = bt.indicators.SimpleMovingAverage(datafeed.close, period = 100);
-            self.myInds[datafeed]["cross"] = bt.indicators.CrossOver(self.myInds[datafeed]["sma40"], self.myInds[datafeed]["sma100"]);
+            self.myInds[datafeed]["sma1"] = bt.indicators.SimpleMovingAverage(datafeed.close, period = self.params.sma1);
+            self.myInds[datafeed]["sma2"] = bt.indicators.SimpleMovingAverage(datafeed.close, period = self.params.sma2);
+            self.myInds[datafeed]["cross"] = bt.indicators.CrossOver(self.myInds[datafeed]["sma1"], self.myInds[datafeed]["sma2"]);
 
     def notify_order(self, order):
         pass
@@ -39,35 +41,35 @@ class TestStrategy(bt.Strategy):
             currentPosition = self.getposition(datafeed);
             if not currentPosition:
 
-                # If the 40 SMA crossed over the 100 SMA then GO LONG!!
+                # If the short SMA crossed over the long SMA then GO LONG!!
                 if self.myInds[datafeed]["cross"][0] == 1:
-                    print(datafeed._name + " - The sma40 just crossed the sma100 - BUY!!!!");
-                    print(datafeed._name + " - The 40 is now: ", self.myInds[datafeed]["sma40"][0]);
-                    print(datafeed._name + " - The 100 is now: ", self.myInds[datafeed]["sma100"][0]);
-                    self.buy(datafeed, size = 10);
+                    print(datafeed._name + " - The sma1 just crossed the sma2 - BUY!!!!");
+                    print(datafeed._name + " - The sma1 is now: ", self.myInds[datafeed]["sma1"][0]);
+                    print(datafeed._name + " - The sma2 is now: ", self.myInds[datafeed]["sma2"][0]);
+                    self.buy(datafeed, size = 100);
 
-                # If the 100 SMA crossed over the 40 SMA then GO SHORT!!
+                # If the long SMA crossed over the short SMA then GO SHORT!!
                 elif self.myInds[datafeed]["cross"][0] == -1:
-                    print(datafeed._name + " - The sma100 just crossed the sma40 - SHORT!!!!");
-                    print(datafeed._name + " - The 40 is now: ", self.myInds[datafeed]["sma40"][0]);
-                    print(datafeed._name + " - The 100 is now: ", self.myInds[datafeed]["sma100"][0]);
-                    self.sell(datafeed, size = 10);
+                    print(datafeed._name + " - The sma2 just crossed the sma1 - SHORT!!!!");
+                    print(datafeed._name + " - The sma1 is now: ", self.myInds[datafeed]["sma1"][0]);
+                    print(datafeed._name + " - The sma2 is now: ", self.myInds[datafeed]["sma2"][0]);
+                    self.sell(datafeed, size = 100);
             else:
-                # If the 40 SMA crossed over the 100 SMA then CLOSE OUT YOUR SHORT AND GO LONG!!
-                if self.myInds[datafeed]["cross"][0] == -1:
-                    print(datafeed._name + " - The sma100 just crossed the sma40 - CLOSE SHORT AND GO LONG!!!!");
-                    print(datafeed._name + " - The 40 is now: ", self.myInds[datafeed]["sma40"][0]);
-                    print(datafeed._name + " - The 100 is now: ", self.myInds[datafeed]["sma100"][0]);
+                # If the short SMA crossed over the long SMA then CLOSE OUT YOUR SHORT AND GO LONG!!
+                if self.myInds[datafeed]["cross"][0] == 1:
+                    print(datafeed._name + " - The sma1 just crossed the sma2 - CLOSE SHORT AND GO LONG!!!!");
+                    print(datafeed._name + " - The sma1 is now: ", self.myInds[datafeed]["sma1"][0]);
+                    print(datafeed._name + " - The sma2 is now: ", self.myInds[datafeed]["sma2"][0]);
                     self.close(datafeed);
-                    self.buy(datafeed, size = 10)
+                    self.buy(datafeed, size = 100)
 
-                # If the 100 SMA crossed over the 40 SMA then CLOSE OUT YOUR LONG AND GO SHORT!!
-                if self.myInds[datafeed]["cross"][0] == -1:
-                    print(datafeed._name + " - The sma100 just crossed the sma40 - CLOSE LONG AND GO SHORT!!!!");
-                    print(datafeed._name + " - The 40 is now: ", self.myInds[datafeed]["sma40"][0]);
-                    print(datafeed._name + " - The 100 is now: ", self.myInds[datafeed]["sma100"][0]);
+                # If the long SMA crossed over the short SMA then CLOSE OUT YOUR LONG AND GO SHORT!!
+                elif self.myInds[datafeed]["cross"][0] == -1:
+                    print(datafeed._name + " - The sma2 just crossed the sma1 - CLOSE LONG AND GO SHORT!!!!");
+                    print(datafeed._name + " - The sma1 is now: ", self.myInds[datafeed]["sma1"][0]);
+                    print(datafeed._name + " - The sma2 is now: ", self.myInds[datafeed]["sma2"][0]);
                     self.close(datafeed);
-                    self.sell(datafeed, size = 10);
+                    self.sell(datafeed, size = 100);
 
 if __name__ == '__main__':
     # Create Cerebro entity
@@ -76,27 +78,26 @@ if __name__ == '__main__':
     # Add Strategy
     cerebro.addstrategy(TestStrategy);
 
-    # Create a Data Feed
-    data1 = bt.feeds.YahooFinanceData(
-        dataname = "ORCL",
-        # Do not pass values before this date
-        fromdate = datetime.datetime(2000, 1, 1),
-        # Do not pass values after this date
-        todate = datetime.datetime(2005, 6, 30),
-        reverse = False
-    );
+    # Create a Data Feeds
+    yahooList = [("ORCL", "Oracle Stock"),
+                    ("AAPL", "Apple Stock"),
+                    ("MSFT", "Microsoft Stock"),
+                    ("INTC", "Intel Stock")
+                ];
 
-    data2 = bt.feeds.YahooFinanceData(
-        dataname = "AAPL",
-        # Do not pass values before this date
-        fromdate = datetime.datetime(2000, 1, 1),
-        # Do not pass values after this date
-        todate = datetime.datetime(2005, 6, 30),
-        reverse = False
-    );
-    # Add the Data Feed to Cerebro
-    cerebro.adddata(data1, "Oracle Stock");
-    cerebro.adddata(data2, "Apple Stock");
+    for stock in yahooList:
+
+        newDataFeed = bt.feeds.YahooFinanceData(
+            dataname = stock[0],
+            # Do not pass values before this date
+            fromdate = datetime.datetime(2000, 1, 1),
+            # Do not pass values after this date
+            todate = datetime.datetime(2010, 1, 1),
+            reverse = False
+        );
+
+        # Add the Data Feed to Cerebro
+        cerebro.adddata(newDataFeed, stock[1]);
 
     # Set our desired cash start
     cerebro.broker.setcash(100000.00);
