fff96d90c362b463241b1e1f368a29ff43e4865c
Joshua Peeling <jpeeling13@outlook.com>
2019-08-05 22:46:18 -0400

    Made tiny improvement to sma cross strategy using 3 smas instead of 2

--- multiplestocks_crossover_strategy.py
+++ multiplestocks_crossover_strategy.py
@@ -9,17 +9,19 @@ import backtrader as bt
 # Create our first test strategy!!
 class TestStrategy(bt.Strategy):
 
-    params = (('sma1', 40),('sma2', 100),);
+    params = (('sma1', 20),('sma2', 100), ('sma3', 200));
 
     def __init__(self):
-        print("Creating sma1, sma2, and CROSS for each Datafeed");
+        print("Creating sma1, sma2, sma3, and CROSS1 and CROSS2 for each Datafeed");
         self.myInds = dict();
 
         for datafeed in self.datas:
             self.myInds[datafeed] = dict();
             self.myInds[datafeed]["sma1"] = bt.indicators.SimpleMovingAverage(datafeed.close, period = self.params.sma1);
             self.myInds[datafeed]["sma2"] = bt.indicators.SimpleMovingAverage(datafeed.close, period = self.params.sma2);
-            self.myInds[datafeed]["cross"] = bt.indicators.CrossOver(self.myInds[datafeed]["sma1"], self.myInds[datafeed]["sma2"]);
+            self.myInds[datafeed]["sma3"] = bt.indicators.SimpleMovingAverage(datafeed.close, period = self.params.sma3);
+            self.myInds[datafeed]["cross1"] = bt.indicators.CrossOver(self.myInds[datafeed]["sma1"], self.myInds[datafeed]["sma2"]);
+            self.myInds[datafeed]["cross2"] = bt.indicators.CrossOver(self.myInds[datafeed]["sma1"], self.myInds[datafeed]["sma3"]);
 
     def notify_order(self, order):
         pass
@@ -27,11 +29,12 @@ class TestStrategy(bt.Strategy):
     def notify_trade(self, trade):
         dt = self.data.datetime.date()
         if trade.isclosed:
-            print('{} {} Closed: PnL Gross {}, Net {}'.format(
-                                                dt,
-                                                trade.data._name,
-                                                round(trade.pnl,2),
-                                                round(trade.pnlcomm,2)))
+            print('{} {} Closed: PnL Gross {}, Net {}'.format(dt, trade.data._name, round(trade.pnl,2), round(trade.pnlcomm,2)))
+
+    def log_line_prices(self, datafeed):
+        print(datafeed._name + " - The sma1 is now: ", self.myInds[datafeed]["sma1"][0]);
+        print(datafeed._name + " - The sma2 is now: ", self.myInds[datafeed]["sma2"][0]);
+        print(datafeed._name + " - The sma3 is now: ", self.myInds[datafeed]["sma3"][0]);
 
     def next(self):
         print(self.data.datetime.date(0));
@@ -41,35 +44,31 @@ class TestStrategy(bt.Strategy):
             currentPosition = self.getposition(datafeed);
             if not currentPosition:
 
-                # If the short SMA crossed over the long SMA then GO LONG!!
-                if self.myInds[datafeed]["cross"][0] == 1:
-                    print(datafeed._name + " - The sma1 just crossed the sma2 - BUY!!!!");
-                    print(datafeed._name + " - The sma1 is now: ", self.myInds[datafeed]["sma1"][0]);
-                    print(datafeed._name + " - The sma2 is now: ", self.myInds[datafeed]["sma2"][0]);
-                    self.buy(datafeed, size = 100);
+                # If the short SMA crossed above the mid SMA then GO LONG!!
+                if self.myInds[datafeed]["cross1"][0] == 1:
+                    print(datafeed._name + " - The sma1 just crossed above the sma2 - BUY!!!!");
+                    self.log_line_prices(datafeed)
+                    self.buy(datafeed, size = 1000);
 
-                # If the long SMA crossed over the short SMA then GO SHORT!!
-                elif self.myInds[datafeed]["cross"][0] == -1:
-                    print(datafeed._name + " - The sma2 just crossed the sma1 - SHORT!!!!");
-                    print(datafeed._name + " - The sma1 is now: ", self.myInds[datafeed]["sma1"][0]);
-                    print(datafeed._name + " - The sma2 is now: ", self.myInds[datafeed]["sma2"][0]);
-                    self.sell(datafeed, size = 100);
+                # If the short SMA crossed under the mid SMA then GO SHORT!!
+                elif self.myInds[datafeed]["cross1"][0] == -1:
+                    print(datafeed._name + " - The sma1 just crossed under the sma2 - SHORT!!!!");
+                    self.log_line_prices(datafeed)
+                    self.sell(datafeed, size = 1000);
             else:
-                # If the short SMA crossed over the long SMA then CLOSE OUT YOUR SHORT AND GO LONG!!
-                if self.myInds[datafeed]["cross"][0] == 1:
-                    print(datafeed._name + " - The sma1 just crossed the sma2 - CLOSE SHORT AND GO LONG!!!!");
-                    print(datafeed._name + " - The sma1 is now: ", self.myInds[datafeed]["sma1"][0]);
-                    print(datafeed._name + " - The sma2 is now: ", self.myInds[datafeed]["sma2"][0]);
+                # If the short SMA crossed over the mid SMA then CLOSE OUT YOUR SHORT AND GO LONG!!
+                if self.myInds[datafeed]["cross1"][0] == 1:
+                    print(datafeed._name + " - The sma1 just crossed above the sma2 - CLOSE SHORT AND GO LONG!!!!");
+                    self.log_line_prices(datafeed)
                     self.close(datafeed);
-                    self.buy(datafeed, size = 100)
+                    self.buy(datafeed, size = 1000)
 
-                # If the long SMA crossed over the short SMA then CLOSE OUT YOUR LONG AND GO SHORT!!
-                elif self.myInds[datafeed]["cross"][0] == -1:
-                    print(datafeed._name + " - The sma2 just crossed the sma1 - CLOSE LONG AND GO SHORT!!!!");
-                    print(datafeed._name + " - The sma1 is now: ", self.myInds[datafeed]["sma1"][0]);
-                    print(datafeed._name + " - The sma2 is now: ", self.myInds[datafeed]["sma2"][0]);
+                # If the short SMA crossed under the long SMA then CLOSE OUT YOUR LONG AND GO SHORT!!
+                elif self.myInds[datafeed]["cross1"][0] == -1:
+                    print(datafeed._name + " - The sma1 just crossed under the sma3 - CLOSE LONG AND GO SHORT!!!!");
+                    self.log_line_prices(datafeed)
                     self.close(datafeed);
-                    self.sell(datafeed, size = 100);
+                    self.sell(datafeed, size = 1000);
 
 if __name__ == '__main__':
     # Create Cerebro entity
